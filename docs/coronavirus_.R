5a448035ec1a9812e943c749e6df7c4a848f28f8
darwinanddavis <matthew.malishev@gmail.com>
2020-05-07 13:05:15 +1000

    rm legend2

--- docs/coronavirus_.R
+++ docs/coronavirus_.R
@@ -499,7 +499,7 @@ cvm <- gcIntermediate(latlon_origin,
   addControl(control_box, "topright", className = "layers-base") %>% 
   addEasyButton(reset_zoom) %>% 
   addEasyButton(locate_me) #%>% 
-  # addControl(html = html_legend, position = "bottomleft")
+  # addControl(html = html_legend, position = "bottomleft") # option 2 legend
   # addControl(legend_box, "bottomleft", className = c("layers-base","full")) %>% 
   # addLegend(colors = "", labels = c(">10,000"), className='full')  
  
