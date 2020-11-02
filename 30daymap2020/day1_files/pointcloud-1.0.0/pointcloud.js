22efaf09143de130777c926fae53201275a1f994
darwinanddavis <matthew.malishev@gmail.com>
2020-11-02 20:00:30 +1100

    30daymap day1

---
+++ 30daymap2020/day1_files/pointcloud-1.0.0/pointcloud.js
@@ -0,0 +1,102 @@
+
+function add_pointcloud_geo_columnar( map_id, map_type, pointcloud_data, data_count, radius, layer_id, light_settings, auto_highlight, highlight_colour, legend, bbox, update_view, focus_layer, js_transition, brush_radius ) {
+
+  var extensions = [];
+
+  if ( brush_radius > 0 ) {
+  	extensions.push( new deck.BrushingExtension() );
+  }
+
+  let hasTooltip = pointcloud_data.tooltip !== undefined;
+
+  //console.log( pointcloud_data );
+
+  const binaryLocation = new Float32Array(pointcloud_data.geometry);
+  const binaryRadius = new Float32Array(pointcloud_data.radius);
+  const binaryFillColour = new Uint8Array(pointcloud_data.fill_colour);
+
+  const pointcloudLayer = new deck.PointCloudLayer({
+  	map_id: map_id,
+    id: 'pointcloud-'+layer_id,
+
+    data: {
+      length: data_count,
+      attributes: {
+        getPosition: {value: binaryLocation, size: 3},
+        getRadius: {value: binaryRadius, size: 1},
+        getColor: {value: binaryFillColour, size: 4},
+      },
+      tooltip: pointcloud_data.tooltip
+    },
+
+    pointSize: radius,
+    lightSettings: light_settings,
+    pickable: true,
+    autoHighlight: auto_highlight,
+    highlightColor: md_hexToRGBA( highlight_colour ),
+    onClick: info => md_layer_click( map_id, "pointcloud", info ),
+    onHover: info => hasTooltip ? md_update_binary_tooltip( info.layer, info.index, info.x, info.y ) : null,
+    transitions: js_transition || {},
+    brushingRadius: brush_radius,
+    extensions: extensions
+  });
+
+  if( map_type == "google_map") {
+	  md_update_overlay( map_id, 'pointcloud-'+layer_id, pointcloudLayer );
+	} else {
+	  md_update_layer( map_id, 'pointcloud-'+layer_id, pointcloudLayer );
+	}
+
+	if (legend !== false) {
+	  md_add_legend(map_id, map_type, layer_id, legend, "rgb" );
+	}
+	md_layer_view( map_id, map_type, layer_id, focus_layer, bbox, update_view );
+}
+
+function add_pointcloud_polyline( map_id, map_type, pointcloud_data, radius, layer_id, light_settings, auto_highlight, highlight_colour, legend, bbox, update_view, focus_layer, js_transition, brush_radius ) {
+
+  var extensions = [];
+
+  if ( brush_radius > 0 ) {
+  	extensions.push( new deck.BrushingExtension() );
+  }
+
+  const pointcloudLayer = new deck.PointCloudLayer({
+    map_id: map_id,
+    id: 'pointcloud-'+layer_id,
+    data: pointcloud_data,
+    pointSize: radius,
+    getPosition: d => decode_pointcloud( d.polyline, d.elevation ),
+    getColor: d => md_hexToRGBA( d.fill_colour ),
+    lightSettings: light_settings,
+    pickable: true,
+    autoHighlight: auto_highlight,
+    highlightColor: md_hexToRGBA( highlight_colour ),
+    onClick: info => md_layer_click( map_id, "pointcloud", info ),
+    onHover: md_update_tooltip,
+    transitions: js_transition || {},
+    brushingRadius: brush_radius,
+    extensions: extensions
+  });
+
+  if( map_type == "google_map") {
+	  md_update_overlay( map_id, 'pointcloud-'+layer_id, pointcloudLayer );
+	} else {
+	  md_update_layer( map_id, 'pointcloud-'+layer_id, pointcloudLayer );
+	}
+
+	if (legend !== false) {
+	  md_add_legend(map_id, map_type, layer_id, legend, "hex" );
+	}
+	md_layer_view( map_id, map_type, layer_id, focus_layer, bbox, update_view );
+}
+
+
+function decode_pointcloud( polyline, elevation ) {
+  var position = [0,0,0];
+  var coords = md_decode_points( polyline );
+  position[0] = coords[0];
+  position[1] = coords[1];
+  position[2] = elevation;
+  return position;
+}
---
+++ docs/30daymap2020/day1.R
@@ -0,0 +1,61 @@
+# --------------------------------------------------------------------
+# 30 day map challenge 2020
+# day 1
+# author: Matt Malishev
+# @darwinanddavis  
+
+# pcks --------------------------------------------------------------------
+require(pacman)
+p_load(mapdeck,readr,purrr,stringr,dplyr,tibble,htmltools,sf,sfheaders,data.table,stringr,tigris,sp,here)
+
+# key_m <- readr::read_lines("<mapdeck key>")
+# set_token(key_m) # mapbox key
+id_df <- here("snackmap","id_df.csv") %>% read_csv() # read in current data
+
+# read in latlon data -----------------------------------------------------
+
+fh <- "coffee"
+m <- paste0("https://github.com/darwinanddavis/worldmaps/blob/gh-pages/data/",fh,".Rdata?raw=true") %>% url %>% readRDS
+m$name <- m$name %>% paste0("\n \n \n") # add linebreaks to names
+m_ttl <- tibble(lon=-43,lat=31, # add title
+                name=paste0(fh %>% stringr::str_to_title(),"\nsnackmap")
+)
+
+# map
+mp <- mapdeck(data=m,
+        location = c(m$lon[1],m$lat[1]), 
+        zoom = 10,
+        pitch =  0,
+        style = id_df %>% filter(Name == fh) %>% pull(Style)
+) %>%
+  add_pointcloud(lon = "lon",lat = "lat", 
+                 layer_id = "latlon",id = "latlon",
+                 fill_colour = id_df %>% filter(Name == fh) %>% pull(Col),
+                 auto_highlight = T, 
+                 highlight_colour = "#FFFFFFFF",
+                 elevation = 1,
+                 radius = 10, 
+                 update_view = T,
+                 tooltip = "info"
+  ) %>% 
+  mapdeck::add_text(lon = "lon", lat = "lat",
+                    layer_id = "label", text = "name",
+                    alignment_baseline = "top",anchor = "end",
+                    fill_colour = id_df %>% filter(Name == fh) %>% pull(Col),
+                    billboard = T,update_view = T,
+                    font_family = "Quicksand",
+                    size=15
+  ) %>% 
+  mapdeck::add_text(m_ttl,lon = "lon", lat = "lat",
+                    layer_id = "title", text = "name",
+                    alignment_baseline = "top",anchor = "end",
+                    fill_colour = id_df %>% filter(Name == fh) %>% pull(Col),
+                    billboard = T, update_view = T,
+                    font_family = "Cinzel",
+                    size=35
+  )
+mp
+mp %>% saveWidget(here::here("worldmaps","30daymap2020","day1.html"))  
+
+
+
