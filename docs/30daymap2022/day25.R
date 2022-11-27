cf899e9f145b7ce798de961b3629038b1bb64a5e
darwinanddavis <matthew.malishev@gmail.com>
2022-11-28 09:02:03 +1100

    30day day25 save

--- docs/30daymap2022/day25.R
+++ docs/30daymap2022/day25.R
@@ -8,53 +8,60 @@
 fh <- "two_colours"
 
 # pcks --------------------------------------------------------------------
-pacman::p_load(sf,here,dplyr,osmdata,ggplot2,colorspace,osrm,osmextract,magrittr,sfheaders,maps,stringr)
+pacman::p_load(sf,here,dplyr,osmdata,ggplot2,colorspace,osrm,osmextract,magrittr,sfheaders,maps,stringr,mapdata)
 
 # get data ----------------------------------------------------------------
 
 # find centroids 
+data(world.cities) # /maps
 prj <- 4326
-site <- c("melbourne","amsterdam","leipzig","atlanta","medellin")
-countries <- c("australia","netherlands","germany","usa","colombia")
+buff <- 30
+site <- c("Melbourne","Amsterdam","Leipzig","Atlanta","Medellin")
+countries <- c("Australia","Netherlands","Germany","USA","Colombia")
 sfcent <- world.cities %>% # get sites 
-  filter(country.etc %in% countries, name %in%  str_to_title(site)) %>% 
+  filter(country.etc %in% countries, name %in% site) %>% 
   dplyr::select(name,long,lat) %>% dplyr::rename(.cols = 2, lon = long) %>% 
-  sfheaders::sf_point(keep = T, x = "lon", y = "lat") 
-st_crs(sfcent) <- prj # set proj
-sfpath <- data.frame(id='center', lon=c(sfcent %>% st_coordinates() %>% .[1],sfcent %>% st_coordinates() %>% .[1] +0.5), lat=c(sfcent %>% st_coordinates() %>% .[2],sfcent %>% st_coordinates() %>% .[2]+0.5)) %>% sfheaders::sfc_linestring(x='lon', y='lat') %>% st_as_sf(crs = prj)
-sfpoly <- sfcent %>% st_buffer(1000) %>% st_cast("POLYGON")
+  slice(-6) %>% 
+  st_as_sf(coords = c("lon", "lat"), crs = 4326)
+sfpoly <- sfcent %>% st_buffer(buff) %>% st_cast("POLYGON")
 
 # osrm  -------------------------------------------------------------------
 dir.create(here::here(fh)); dir.create(here::here(fh,"data",site)); dir.create(here::here(fh,"plot",site))
-
-library(osmdata)
-bb <- sfpoly %>% st_bbox()
-get_osm <- function(bb,key,value,type) opq(bbox = bb) %>%
-  add_osm_feature(key = key, value = value) %>%
-  osmdata_sf() %>% .[[type]]
+require(osmdata)
+saveosm <- function(id,f) saveRDS(here::here(fh,"data",site,paste(id,f)))
 
 # roads
 o1 <- c("motorway","primary","trunk")
 o2 <- "secondary"
 o3 <- c("tertiary","footpath","sidewalk","pedestrian","living_street")
 
-osm_primary <- get_osm(bb,"highway",o1,"osm_lines")
-osm_secondary <- get_osm(bb,"highway",o2,"osm_lines")
-osm_tertiary <- get_osm(bb,"highway",o3,"osm_lines")
-
-# save to dir
-osm_primary %>% saveRDS(here::here(fh,"data",site,paste("osm_primary.Rda")))
-osm_secondary %>% saveRDS(here::here(fh,"data",site,paste("osm_secondary.Rda")))
-osm_tertiary %>% saveRDS(here::here(fh,"data",site,paste("osm_tertiary.Rda")))
+# get osm data for cities
+for(id in sfpoly %>% nrow){
+  bb <- sfpoly[id,] %>% st_bbox()
+  get_osm <- function(bb,key,value,type) opq(bbox = bb) %>%
+    add_osm_feature(key = key, value = value) %>%
+    osmdata_sf() %>% .[[type]]
+  
+  osm_primary <- get_osm(bb,"highway",o1,"osm_lines")
+  osm_secondary <- get_osm(bb,"highway",o2,"osm_lines")
+  osm_tertiary <- get_osm(bb,"highway",o3,"osm_lines")
+  
+  # save to dir
+  osm_primary %>% saveosm(id,"osm_primary.Rda")
+  osm_secondary %>% saveosm(id,"osm_secondary.Rda")
+  osm_tertiary %>% saveosm(id,"osm_tertiary.Rda")
+}
 
 # plot --------------------------------------------------------------------
+name <- "Medellin"
+id <- sfpoly[sfpoly$name == name,]
 width = 20
 height = 40
 opac <- 0.8
 cex <- 8
-size <- 0.4  
-bg <- "#262626"; fg <- "#FFFFFF"; col_na <- "#82093E"
-colv <- c("#C36518","#573C78","#6F8E2F","#275791","#000000")
+size <- 1.2  
+bg <- "#FFFFFF"; col_na <- "#000000"
+colpal <- c("#C36518","#573C78","#6F8E2F","#275791","#000000")
 my_theme <-  theme(panel.grid.major = element_line(colour = NA),
                    panel.grid.minor = element_line(colour = NA),
                    panel.background = element_rect(fill = bg, colour = NA),
@@ -64,35 +71,31 @@ my_theme <-  theme(panel.grid.major = element_line(colour = NA),
                    legend.background = element_rect(fill = bg, colour = "transparent"),
                    legend.box.background = element_rect(fill = bg, colour = "transparent"))
 
+# read in data and add colpal 
+df <- here::here("data","site",name) %>% list.files(full.names = T) %>% readRDS() %>% 
+  rbind(
+    osm_primary,
+    osm_secondary,
+    osm_tertiary
+  ) %>% mutate(colpal = colpal[id])
 
-df <- rbind(
-  osm_primary,
-  osm_secondary,
-  osm_tertiary
-)
-
+# optional title
 ttldf <- tibble(label = site %>% str_to_upper() %>% str_sub(0,3),
                 x = bb %>% unlist %>% .[1] + 0.01,
                 y = bb %>% unlist %>% .[4] - 0.02,
-                angle = 90, color = colpal[1], label.color = NA, fill = NA
+                angle = 90, color = "#000000", label.color = NA, fill = NA
 )
 
-colpal <- sequential_hcl(df[var1] %>% n_distinct(), colv)
-
-var1 <- "max"
-var2 <- ""
-size <- 0.4
-colpal <- sequential_hcl(df[var1] %>% n_distinct(), "PinkYl") 
+# plot 
 ggplot() + 
-  geom_sf(data = df %>% filter(highway %in% o1), aes(col = .data[[var1]], fill = .data[[var1]]),size = size) +
-  geom_sf(data = df %>% filter(highway %in% o2), aes(col = .data[[var1]], fill = .data[[var1]]),size = size/2) +
-  geom_sf(data = df %>% filter(highway %in% o3), aes(col = .data[[var1]], fill = .data[[var1]]),size = size/3) +
-  scale_fill_gradientn(name = paste0(ttl,"\n"), colours = adjustcolor(colpal,opac),aesthetics = c("col","fill"), na.value = col_na) +
+  geom_sf(data = df %>% filter(highway %in% o1), aes(fill = colpal, col = colpal), size = size) +
+  geom_sf(data = df %>% filter(highway %in% o2), aes(fill = colpal, col = colpal), size = size/2) +
+  geom_sf(data = df %>% filter(highway %in% o3), aes(fill = colpal, col = colpal), size = size/3) +
   geom_sf(data = sfcent, col = col_na, fill = adjustcolor(col_na,opac), size = 2) +
-  ggspatial::annotation_scale(location = 'bl',bar_cols =c(bg,fg), line_width = 0.5, pad_x = unit(1.5, "cm"), pad_y = unit(0.1, "cm"), text_col = fg, line_col = fg, style = "ticks", width_hint = 0.1) +
+  # ggspatial::annotation_scale(location = 'bl',bar_cols =c(bg,fg), line_width = 0.5, pad_x = unit(1.5, "cm"), pad_y = unit(0.1, "cm"), text_col = fg, line_col = fg, style = "ticks", width_hint = 0.1) + # add scale bar
   ggthemes::theme_map() +
   theme(legend.background = element_rect(fill="transparent", color = "transparent")) +
   coord_sf(xlim = bb[c(1,3)], bb[c(2,4)],crs = prj) +
   my_theme 
 
-ggsave(here::here(fh,"plot",paste(sites[1],".png", sep ="_")), device = "png", width = width, height = height, units = "cm", dpi = "retina", bg = "transparent")
+ggsave(here::here(fh,"plot",paste(df[1],".png", sep ="_")), device = "png", width = width, height = height, units = "cm", dpi = "retina", bg = "transparent")
--- docs/30daymap2022/day4.R
+++ docs/30daymap2022/day4.R
@@ -1,23 +1,25 @@
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ##                            #30daymapchallenge2022                        ----
-##                                day 4 - green                             ~~~~
+##                                day 26 - islands                             ~~~~
 ##                            author: Matt Malishev                         
 ##                               @darwinanddavis                            
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-fh <- "day4"
+fh <- "day26"
 citation <- "https://senseable.mit.edu/treepedia/"
 
 # pcks ----------------------------------------------------------
-pacman::p_load(here,ggplot2,stringr,dplyr,tibble,readr,readr,sf,sfheaders,raster,ggmap,colorspace,concaveman,ggspatial,colorspace,ggspatial)
+pacman::p_load(here,ggplot2,stringr,dplyr,tibble,readr,readr,sf,sfheaders,raster,ggmap,colorspace,concaveman,ggspatial,colorspace,ggspatial,scales,foreign,ggtext,showtext,patchwork)
+
 
 slist_boundaries <- readRDS(here::here("data","30daymap2022","day4_boundaries.Rda"))
 slist_points <-  readRDS(here::here("data","30daymap2022","day4_points.Rda"))
 
 # data ----------------------------------------------------------
-dr <- "/Users/malishev/Documents/Data/worldmaps/30daymapchallenge/2022/day4/data"
-labels <- fhs %>% str_remove_all(paste0(dr,"/greenview")) %>% str_remove_all("_|.json|.geojson")
+# dr <- here::here("data","30daymap2022")
+dr <- "/Users/malishev/Documents/Data/worldmaps/30daymapchallenge/2022/day26/data"
 fhs <-  dr %>% list.files(full.names = T)
+labels <- fhs %>% str_remove_all(paste0(dr,"/greenview")) %>% str_remove_all("_|.json|.geojson")
 prj <- 4326
 slist_points <- st_sfc(crs = prj)
 # slist_boundaries <- st_sfc(crs = prj)
@@ -38,45 +40,105 @@ slist_boundaries <- slist_boundaries %>% mutate("id" = labels,
 slist_boundaries %>% saveRDS(here::here("data","30daymap2022","day4_boundaries.Rda"))
 slist_points %>% saveRDS(here::here("data","30daymap2022","day4_points.Rda"))
 
-
+# add font
+newfonts <- "/Fonts/ADAYWITHOUTSUN.otf"
+fontlib <- "adaywithoutsun"
+font_add(fontlib,regular = newfonts,bold = newfonts)
+showtext_auto(enable = T) # auto showtext
 
 # plot ----------------------------------------------------------
-fg <- "#23351C"
-bg <- "#D3D6ED" %>% lighten(0.3)
+fg <- "#DAF7A6"
+fg2 <- "#5B8017"
+bg <- "#23351C"
+border <- bg %>% lighten(0.2)
 opac <- 0.5
+c(fg,fg2,bg,border) %>% scales::show_col()
+family <- fontlib
+width <- 15; height <- width
+size <-  40
 
 my_theme <- theme(
-  plot.background = element_rect(fill = bg),
-  panel.background = element_rect(fill = bg),
-  text = element_text(color = fg),
+  plot.background = element_rect(fill = bg, color = "transparent"),
+  panel.background = element_rect(fill = bg, color = "transparent"),
+  text = element_text(family = family, color = fg),
+  legend.title = element_text(family = family,colour = fg),
+  legend.text = element_text(family = family,colour = fg),
   legend.background = element_rect(fill = bg),
-  axis.text = element_text(color = fg),
+  axis.text = element_text(color = "transparent"),
+  axis.ticks = element_line(color = "transparent"),
   panel.grid.major = element_line(color = "transparent"),
   panel.grid.minor = element_line(color = "transparent"),
   margin(0,0,0,0,"pt")
 )
 
+# colpal
+colv <- colorRampPalette(colors = c(fg, fg2))
+colpal <- colv(df_bound$id  %>% n_distinct())
+colpal %>% show_col()
 
 # sort by area
-df <- slist_boundaries %>% arrange(area) 
-buff <- df %>% top_n(n = 1, wt = area) %>% st_buffer(1) # set buff to equal largest city
+df_bound <- slist_boundaries %>% arrange(area) %>% 
+  mutate("colpal" = colpal)
+df_point <- slist_points[df_bound$id]
+buff <- df_bound %>% top_n(n = 1, wt = area) %>% st_buffer(1) # set buff to equal largest city
 
-colpal <- sequential_hcl(
-  1,
-  # slist_boundaries %>% n_distinct(),
-  "Dark Mint")
-ggplot() +
-  geom_sf(data = slist_boundaries[1], aes(group = id,  col = colpal, fill = fg %>% adjustcolor(opac)),size = 0.2) +
-  geom_sf(data = slist_boundaries %>% filter(id == "kobe"), size = 0.2, col = fg, fill = fg %>% adjustcolor(opac)) +
-  # geom_sf(data = slist_points[[1]], size = 0.05, col = fg) +
-  ggspatial::annotation_scale(location = 'bl',bar_cols = c(bg,fg), line_width = 0.5, pad_x = unit(0.5, "cm"), pad_y = unit(0.1, "cm"), text_col = fg, line_col = fg, style = "bar" , width_hint = 0.1) +
-  facet_wrap(~id) +
+df_bound[3,] %>% st_bbox
+
+# df_bound %>% 
+#   mutate("x" =  bb[1]+1.5,
+#          "y" =  bb[2]+1.5
+#          ) 
+
+for(nid in df_bound$id[1:3]){
+p <- ggplot() +
+  # geom_sf(data = slist_boundaries, aes(group = id,  col = colpal, fill = fg %>% adjustcolor(opac)),size = 0.2) +
+  geom_sf(data = df_bound %>% filter(id == nid), aes(col = colpal %>% factor, fill = colpal %>% factor %>% adjustcolor(opac),group = id), size = 0.2) +
+  geom_sf(data = df_point[[nid]], size = 0.001, col = fg ) +
+  ggspatial::annotation_scale(location = 'bl',bar_cols = c("transparent",fg), line_width = 0.3, pad_x = unit(0.5, "cm"), pad_y = unit(0.1, "cm"), text_col = fg, line_col = fg, style = "bar" , width_hint = 0.1, text_family = family,text_face = "bold") +
+  # geom_sf_label(data=df_bound %>% filter(id == nid), aes(label=id %>% str_to_upper),family=family, color=fg, size = size, label.size = 0, fill = NA,nudge_x = -0.02, nudge_y = -0.02) +
+  # annotate("text", 0, 0, label = nid,family = family, size = size) +
+  # geom_richtext(data=df_bound %>% filter(id == nid),
+  #               aes(x,y,label = id %>% str_to_upper()), show.legend = F,
+  #               family = family, color=fg, size = 10) +
+  # facet_wrap(~id) +
   # coord_sf(xlim = bb[c(1,3)], ylim = bb[c(1,3)]) +
-  my_theme + 
-  theme_nothing()
+  my_theme  +
+  theme_nothing() 
+  
+  
+p <- p +
+  plot_annotation(subtitle =  nid %>% str_to_upper(),
+                theme = theme(plot.background = element_rect(fill = "transparent", color = "transparent"),
+                              plot.title = element_text(family = family,size = size, color = fg,vjust = 1, hjust = 1),
+                              text = element_text(family = family,size = size,color = fg))) 
+
+dr2 <- "/Users/malishev/Documents/Data/worldmaps/30daymapchallenge/2022/day26/"
+ggsave(paste0(dr2,nid,".png"),p,device = "png", width = width, height = height, units = "cm", dpi = "print")
+}
+
+
+s <- "/Users/malishev/Documents/Data/worldmaps/worldmaps/data/30daymap2022/greenview_tampa.json" %>% 
+  read_sf() %>% dplyr::select(geometry) 
+ss <- s %>% concaveman(concavity = 1)
+
+
+ggplot() +
+  geom_sf(data = slist_boundaries %>% filter(id == "tampa")) +
+  geom_sf(data = slist_points$tampa, size = 0.001, col = fg) +
+  my_theme  +
+  theme_nothing() 
+
+
+
+
+
+
+
+# .. ------------------------------------------------------------
+
 
 # convert to spdf
-spp <- slist_boundaries[1:4,] %>% as("Spatial")
+spp <- slist_boundaries[1:2,] %>% as("Spatial")
 ggplot() +
   geom_spatial_tile(data = spp) + # 
   facet_wrap(~id, scales = "free")
@@ -86,7 +148,8 @@ tm_shape() +
   tm_polygons('attacks', title = 'PKO targeting\nevents (logged)') +
   tm_facets('NAME_0')
 
-slist_boundaries[6:8,] %>%
+require(tmap)
+slist_boundaries[1:2,] %>% 
   tm_shape() +
   tm_borders(group = "id") +
   # tm_polygons('attacks', title = 'PKO targeting\nevents (logged)') +
